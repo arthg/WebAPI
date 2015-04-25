9f130d921b7533e49bd793c50d99e8f876035a03
Arthur Gorr <art.gorr@gmail.com>
2015-04-24 21:36:57 -0500

    WebAPI on OWIN

---
+++ WinAPI/Controllers/ChapterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WinAPI.Controllers
+{
+    public class ChapterController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new []
+                   {
+                       "Owin",
+                       "Katana",
+                       "Web Frameworks",
+                       "Azure",
+                       "Custom Middleware",
+                       "Authentication",
+                       "Appendixes"
+                   };
+        }
+    }
+}
---
+++ WinAPI/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WinAPI.Startup))]
+
+namespace WinAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "default",
+                "api/{controller}",
+                new {controller = "Chapter"}
+                );
+            config.Formatters.XmlFormatter.UseXmlSerializer = true;
+            config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+            //add the WebAPI framework in the OWIN pipeline:
+            app.UseWebApi(config);
+        }
+    }
+}
